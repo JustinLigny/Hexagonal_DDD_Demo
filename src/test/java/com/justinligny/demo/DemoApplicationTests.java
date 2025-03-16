86cf2dd67f2f5c9b6710df8201235f9bc103ab4c
Justin Ligny <justin.ligny@gmail.com>
2025-03-16 12:41:11 +0100

    Initial commit

---
+++ src/test/java/com/justinligny/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinligny.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
